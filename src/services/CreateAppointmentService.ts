e1bfa9721f7137ea58f5288f649714fdca7946b7
christian-oliveira <christianoliveirati@gmail.com>
2020-09-07 02:25:42 -0300

    integrando Entities aos models e repositories e refinando os services para usar os repositories

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -9,22 +10,23 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({ provider, date }: RequestDTO): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentSameDate) {
             throw Error("This appointment is already booked.");
         }
 
-        const appointment = this.appointmentsRepository.create({ provider, date: appointmentDate });
+        const appointment = appointmentsRepository.create({ 
+            provider,
+            date: appointmentDate 
+        });
+
+        await appointmentsRepository.save(appointment);
         
         return appointment;
     }
