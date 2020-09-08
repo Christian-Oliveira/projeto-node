ca420e677dd9ee6588a9a360a05b521ece43f6d5
christian-oliveira <christianoliveirati@gmail.com>
2020-09-07 22:04:46 -0300

    criando relationship de Appointments com Users

--- src/database/migrations/1599510369346-CreateUsers.ts
+++ src/database/migrations/1599510369346-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1599510369346 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
