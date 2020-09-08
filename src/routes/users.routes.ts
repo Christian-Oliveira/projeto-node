fca6363232325993cd3350c959a390673e20d785
christian-oliveira <christianoliveirati@gmail.com>
2020-09-07 23:25:08 -0300

    criando route e service de Users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+import { Router } from 'express';
+
+import User from '../models/User';
+import CreateUserService from '../services/CreateUserService';
+
+
+const usersRouter = Router();
+
+usersRouter.get('/', async (request, response) => {
+    const usersRepository = getRepository(User);
+    const users = await usersRepository.find();
+
+    return response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name, email, password,
+        });
+
+        return response.json(user);
+    } catch (e) {
+        return response.status(400).json({ error: e.message });
+    }
+});
+
+export default usersRouter;
